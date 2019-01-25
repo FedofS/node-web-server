9f10674bb9614209af2e724800f763514cbbb883
Sergey Fedof <7ff77ff@gmail.com>
2019-01-25 19:51:23 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,63 @@
+// nodemon server.js -e js,hbs
+
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+// промежуточное ПО останавливает запуск
+app.use((req, res, next) => {
+  res.render('maitenanse.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>')
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/bad', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>')
+  res.send({
+    errorMessage: 'Unable to handle request',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
